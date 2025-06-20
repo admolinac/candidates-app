6df9c2f5035a4054f038c5584a22ec0420ec5348
Angie Molina <amolina1795@gmail.com>
2025-06-19 22:06:02 -0500

    Add format code in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-  ],
+    plugins: [
+        tsconfigPaths(),
+        TanStackRouterVite({ autoCodeSplitting: true }),
+        viteReact(),
+    ],
 });
