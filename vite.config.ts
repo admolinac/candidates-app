8a296bae6a85c10f139b0f19cc39892c6287bef6
Angie Molina <amolina1795@gmail.com>
2025-06-22 20:19:45 -0500

    feat: integrate Tailwind CSS and utility functions
    - Added Tailwind CSS and related dependencies to package.json.
    - Imported Tailwind CSS in Candidate.css for styling.
    - Created a utility function `cn` in style.ts to merge class names using clsx and tailwind-merge.
    - Updated vite.config.ts to include Tailwind CSS plugin for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
         tsconfigPaths(),
         TanStackRouterVite({ autoCodeSplitting: true }),
         viteReact(),
+        tailwindcss(),
     ],
 });
